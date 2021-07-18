50632204db388b2be6d2e5c6bfea018e067aa00e
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-07-18 16:09:57 +0900

    Add File Read Example

--- src/demo/TryDemoNIO.java
+++ src/demo/TryDemoNIO.java
@@ -1,7 +1,13 @@
 package demo;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -19,6 +25,9 @@ public class TryDemoNIO {
 		List<Runnable> list = new ArrayList<Runnable>();
 		list.add(TryDemoNIO::tryFileToPath);
 		list.add(TryDemoNIO::tryPathGetRoot);
+		list.add(TryDemoNIO::tryReadFileNotFound);
+		list.add(TryDemoNIO::tryReadFile);
+		list.add(TryDemoNIO::tryReadFileAll);
 		for (var r : list) {
 			r.run();
 		}
@@ -36,6 +45,52 @@ public class TryDemoNIO {
 		Path path = Paths.get("sample.txt");
 		
 		System.out.println(path.getRoot());
+	}
+	
+	private static void tryReadFileNotFound() {
+		System.out.println("******* TryReadFileNotFound *******");
+		
+		Path path = Paths.get("notfound.txt");
+		try (InputStream in = Files.newInputStream(path); BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+		    String line = null;
+		    while ((line = reader.readLine()) != null) {
+		        System.out.println(line);
+		    }
+		} catch (NoSuchFileException  e) {
+		    System.err.println("File does not exist. File: " + e.getFile());
+		} catch (IOException  e) {
+		    System.err.println(e);
+		}
+
+	}
+	
+	private static void tryReadFileAll() {
+		System.out.println("******* TryReadFileAll *******");
+		
+		try {
+			Path path = Paths.get("sample.txt");
+			String contents = Files.readString(path, Charset.forName("UTF-8"));
+			System.out.println(contents);
+		} catch (IOException  e) {
+		    System.err.println(e);
+		}
+	}
+	
+	private static void tryReadFile() {
+		System.out.println("******* TryReadFile *******");
+		
+		Path path = Paths.get("sample.txt");
+		try (InputStream in = Files.newInputStream(path); BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+		    String line = null;
+		    while ((line = reader.readLine()) != null) {
+		        System.out.println(line);
+		    }
+		} catch (NoSuchFileException  e) {
+		    System.err.println("File does not exist. File: " + e.getFile());
+		} catch (IOException  e) {
+		    System.err.println(e);
+		}
+
 	}
 	
 }
